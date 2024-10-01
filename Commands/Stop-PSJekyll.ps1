c9be728592463881bb159271c0a885b79785bc63
James Brundage <+@noreply.github.com>
2024-09-30 19:48:43 -0700

    feat: Stop-PSJekyll ( Fixes #6 )

---
+++ Commands/Stop-PSJekyll.ps1
@@ -0,0 +1,20 @@
+function Stop-PSJekyll {
+    <#
+    .SYNOPSIS
+        Stops a Jekyll server
+    .DESCRIPTION
+        Stops a Jekyll server in a PowerShell job.
+    .LINK
+        https://jekyllrb.com/
+    #>
+    [Alias('Stop-Jekyll')]   
+    param(
+    # The name of the Jekyll job
+    [string]
+    $Name = '*'
+    )
+
+    process {
+        Get-Job -Name "Jekyll.$Name" | Stop-Job
+    }
+}
