560a82a4e33a5e3a638e9d0322cea1b759a0bcc7
James Brundage <+@noreply.github.com>
2024-10-03 20:01:30 -0700

    feat: PSJekyll.Site.Page ( Fixes #45 )
    Adjusting pattern

--- Types/PSJekyll.Site/get_Page.ps1
+++ Types/PSJekyll.Site/get_Page.ps1
@@ -1,4 +1,4 @@
-foreach ($specialFile in $this.File -notmatch '[\\/]_.+?[\\/]' -match '(?>\.md|\.markdown|.\html)$') {
+foreach ($specialFile in $this.File -notmatch '[\\/]_.+?[\\/]' -match '\.(?>md|markdown|html?)$') {
     $specialFile.pstypenames.add("PSJekyll.Page")    
     $specialFile
 }
