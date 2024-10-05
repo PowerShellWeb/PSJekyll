80ef47ed91781c5e0bfe17f848ee8184fab346ea
James Brundage <+@noreply.github.com>
2024-10-04 23:19:18 -0700

    fix: PSJekyll tests ( Fixes #54 )
    Installing ruby and jekyll in test, with more tracing

--- PSJekyll.tests.ps1
+++ PSJekyll.tests.ps1
@@ -1,9 +1,13 @@
 describe PSJekyll {
     beforeAll {
-        if ($PSVersionTable.OS -match 'Nix') {
-            sudo apt-get install ruby-full
-            sudo gem install jekyll 
-        }
+        $jekyllInPath =  $ExecutionContext.SessionState.InvokeCommand.GetCommand('jekyll', 'Application')
+        if (-not $jekyllInPath -and $env:GITHUB_WORKFLOW) {
+            "::group::Installing FFMpeg" | Out-Host
+            sudo apt update | Out-Host            
+            sudo apt install ruby-full -y | Out-Host
+            sudo gem install jekyll | Out-Host        
+            "::endgroup::" | Out-Host
+        }                                
     }
     context 'New-Jekyll' {
         it 'Will create a jekyll site' {
