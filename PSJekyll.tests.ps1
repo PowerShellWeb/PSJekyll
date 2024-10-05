4ed798987738e1d53fff75b10c9878703d7466ac
James Brundage <+@noreply.github.com>
2024-10-04 23:12:36 -0700

    feat: PSJekyll tests ( Fixes #54 )
    Waiting for job

--- PSJekyll.tests.ps1
+++ PSJekyll.tests.ps1
@@ -3,6 +3,10 @@ describe PSJekyll {
         it 'Will create a jekyll site' {
             $siteName = "MyRandomSite$(Get-Random)"
             $creatingSite = New-PSJekyll -Name $siteName
+            if ($creatingSite -is [Management.Automation.Job]) {
+                $creatingSite | Wait-Job
+                $creatingSite = $creatingSite | Receive-Job
+            }
             Push-Location $siteName
             $PSJekyll.CurrentSite.SiteName | Should -Be $siteName
             Pop-Location
