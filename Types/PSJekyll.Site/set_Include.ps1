5a5605413ac0eeace52190753234d3dc6abe699d
James Brundage <+@noreply.github.com>
2024-10-04 23:03:21 -0700

    feat: PSJekyll.Site.set_Include ( Fixes #50 )

---
+++ Types/PSJekyll.Site/set_Include.ps1
@@ -0,0 +1,39 @@
+param()
+
+$unrolledArguments = @($args | . { process { $_ } })
+
+filter toFileName {
+    $_ -replace '[<>\|\?\*:]', '-' -replace '\s','-'
+}
+
+$name, $content, $metdata = $null, $null, $null
+
+foreach ($arg in $unrolledArguments) {
+    if ($arg -is [Collections.IDictionary]) {
+        $metadata = $arg        
+    }
+    elseif ($arg -is [string] -and -not $name) {
+        $Name = $arg
+    }
+    elseif ($arg -is [string] -and -not $content) {
+        $content = $arg            
+    }
+}
+
+if (-not $metadata) {
+    $metadata = [Ordered]@{}
+}
+if (-not $metadata.title) {
+    $metadata.title = $Name
+}
+
+
+
+Set-Content -Path (
+    $this.Directory,"_includes",($Name | toFileName) -join ([IO.Path]::DirectorySeparatorChar) -replace '^\\'
+) -Value $(
+    @(
+        $metadata | & $psJekyll.FormatYaml.Script -YamlHeader
+        $content
+    ) -join [Environment]::NewLine
+)
