b926e42d5bbbb5d03c79c0693e89c8e844354b25
James Brundage <+@noreply.github.com>
2024-10-06 14:25:53 -0700

    feat: PSJekyll.Template.IncludeGoogleFont ( Fixes #59 )
    Adding codefont, defaulting to Roboto

--- Types/PSJekyll.Template/IncludeGoogleFont.ps1
+++ Types/PSJekyll.Template/IncludeGoogleFont.ps1
@@ -10,7 +10,10 @@
 param(
 # The name of the font to include.
 [string]
-$FontName
+$FontName,
+
+[string]
+$CodeFont
 )
 if ($FontName) {
     "<link href='https://fonts.googleapis.com/css?family=$fontName' rel='stylesheet'>"
@@ -19,6 +22,23 @@ if ($FontName) {
         "{% if site.googleFont %}"
             "<link href='https://fonts.googleapis.com/css?family={{site.googleFont}}' rel='stylesheet'>"
             "<style type='text/css'>body { font-family: '{{site.googleFont}}',sans-serif } </style>"
+        "{% else %}"
+            "<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>"
+            "<style type='text/css'>body { font-family: 'Roboto',sans-serif } </style>"
+        "{% endif %}"
+    ) -join [Environment]::Newline
+}
+
+if ($CodeFont) {
+    "<link href='https://fonts.googleapis.com/css?family=$CodeFont' rel='stylesheet'>"
+} else {
+    @(
+        "{% if site.codeFont %}"
+            "<link href='https://fonts.googleapis.com/css?family={{site.codeFont}}' rel='stylesheet'>"
+            "<style type='text/css'>code, pre { font-family: '{{site.codeFont}}',monospace } </style>"
+        "{% else %}"
+            "<link href='https://fonts.googleapis.com/css?family=Roboto+Mono' rel='stylesheet'>"
+            "<style type='text/css'>code, pre { font-family: 'Roboto Mono',monospace } </style>"
         "{% endif %}"
     ) -join [Environment]::Newline
 }
