5c71492cd9b1682e844294dd643c284513370e12
James Brundage <+@noreply.github.com>
2024-10-06 14:40:53 -0700

    feat: PSJekyll.Template.IncludeGoogleFont ( Fixes #59 )
    Returning as single string.

--- Types/PSJekyll.Template/IncludeGoogleFont.ps1
+++ Types/PSJekyll.Template/IncludeGoogleFont.ps1
@@ -15,24 +15,22 @@ $FontName,
 [string]
 $CodeFont
 )
+@(
 if ($FontName) {
     "<link href='https://fonts.googleapis.com/css?family=$fontName' rel='stylesheet'>"
-} else {
-    @(
-        "{% if site.googleFont %}"
-            "<link href='https://fonts.googleapis.com/css?family={{site.googleFont}}' rel='stylesheet'>"
-            "<style type='text/css'>body { font-family: '{{site.googleFont}}',sans-serif } </style>"
-        "{% else %}"
-            "<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>"
-            "<style type='text/css'>body { font-family: 'Roboto',sans-serif } </style>"
-        "{% endif %}"
-    ) -join [Environment]::Newline
+} else {    
+    "{% if site.googleFont %}"
+        "<link href='https://fonts.googleapis.com/css?family={{site.googleFont}}' rel='stylesheet'>"
+        "<style type='text/css'>body { font-family: '{{site.googleFont}}',sans-serif } </style>"
+    "{% else %}"
+        "<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>"
+        "<style type='text/css'>body { font-family: 'Roboto',sans-serif } </style>"
+    "{% endif %}"    
 }
 
 if ($CodeFont) {
     "<link href='https://fonts.googleapis.com/css?family=$CodeFont' rel='stylesheet'>"
-} else {
-    @(
+} else {    
         "{% if site.codeFont %}"
             "<link href='https://fonts.googleapis.com/css?family={{site.codeFont}}' rel='stylesheet'>"
             "<style type='text/css'>code, pre { font-family: '{{site.codeFont}}',monospace } </style>"
@@ -40,6 +38,6 @@ if ($CodeFont) {
             "<link href='https://fonts.googleapis.com/css?family=Roboto+Mono' rel='stylesheet'>"
             "<style type='text/css'>code, pre { font-family: 'Roboto Mono',monospace } </style>"
         "{% endif %}"
-    ) -join [Environment]::Newline
 }
+) -join [Environment]::Newline
 
