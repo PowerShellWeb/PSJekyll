6cc287401704184014325cfc728ad7a5cdc7a0b5
James Brundage <+@noreply.github.com>
2024-10-05 18:29:18 -0700

    feat: PSJekyll.CurrentSite.get/set_Config ( Fixes #65, Fixes #66 )

---
+++ Types/PSJekyll.Site/get_Config.ps1
@@ -0,0 +1,13 @@
+<#
+.SYNOPSIS
+    Gets the config of the site.
+.DESCRIPTION
+    Gets the configuration of the Jekyll site.  
+    
+    This can be provided by a _config.yml file in the root of the site (and essentially marks it as a site)
+#>
+
+$configFile = Join-Path $this.Directory "_config.yml"
+if (Test-Path $configFile) {
+    Get-Content -Path $configFile
+}
---
+++ Types/PSJekyll.Site/set_Config.ps1
@@ -0,0 +1,25 @@
+<#
+.SYNOPSIS
+    Gets the config of the site.
+.DESCRIPTION
+    Gets the configuration of the Jekyll site.  
+    
+    This can be provided by a _config.yml file in the root of the site (and essentially marks it as a site)
+#>
+param(
+$Value
+)
+$configFile = Join-Path $this.Directory "_config.yml"
+$valueToAdd = 
+    if ($value -is [string]) {
+        $value
+    } else {
+        $PSJekyll.FormatYAML($value)
+    }
+if (Test-Path $configFile) {    
+    if ($valueToAdd) {
+        Add-Content -Path $configFile -Value $valueToAdd
+    }
+} else {
+    New-Item -ItemType File -Path $configFile -Force -Value $valueToAdd
+}
