4ba45d8bfcb536469e38560cd7dac97fab40f093
James Brundage <+@noreply.github.com>
2024-10-06 11:16:14 -0700

    feat: PSJekyll initial example ( Fixes #31 )
    Adding permalink: pretty and removing test error condition

--- PSJekyll.PSJekyll.ps1
+++ PSJekyll.PSJekyll.ps1
@@ -1,14 +1,15 @@
 # PSJekyll site self creation
 $sitePath = Join-Path $PSScriptRoot 'docs'
 Push-Location $sitePath
-$PSJekyll.CurrentSite2.Domain = "psjekyll.powershellweb.com" # this should error, but not fail
+$PSJekyll.CurrentSite.Domain = "psjekyll.powershellweb.com"
 $PSJekyll.CurrentSite.Data = @{LastDateBuilt = [datetime]::UtcNow.Date.ToString('yyyy-MM-dd')}
 $PSJekyll.CurrentSite.Data
 $PSJekyll.CurrentSite.Config = @{
     title = "PSJekyll"
     description = "A PowerShell module for creating Jekyll sites."
     baseurl = "/"
-    url = "https://psjekyll.powershellweb.com"    
+    url = "https://psjekyll.powershellweb.com"
+    permalink = 'pretty'
 }
 $PSJekyll.CurrentSite.Config
 
