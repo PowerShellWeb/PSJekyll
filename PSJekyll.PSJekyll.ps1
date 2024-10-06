3fe31d9cc123a2a66add6499d756dc15ef682b98
James Brundage <+@noreply.github.com>
2024-10-06 10:58:51 -0700

    feat: PSJekyll initial example ( Fixes #31 )
    Testing error behavior (removing throw)

--- PSJekyll.PSJekyll.ps1
+++ PSJekyll.PSJekyll.ps1
@@ -3,7 +3,6 @@ $sitePath = Join-Path $PSScriptRoot 'docs'
 Push-Location $sitePath
 $PSJekyll.CurrentSite2.Domain = "psjekyll.powershellweb.com" # this should error, but not fail
 $PSJekyll.CurrentSite.Data = @{LastDateBuilt = [datetime]::UtcNow.Date.ToString('yyyy-MM-dd')}
-throw "Testing throw" # this should fail the build
 $PSJekyll.CurrentSite.Data
 $PSJekyll.CurrentSite.Config = @{
     title = "PSJekyll"
