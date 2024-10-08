e4604ac0dbad959ff5ae14c8e98659025784fb6b
James Brundage <+@noreply.github.com>
2024-10-07 21:40:26 -0700

    feat: PSJekyll.Template.IncludeImportMap ( Fixes #68 )
    Letting page, site, or data declare an import map.  Being alias happy.

--- Types/PSJekyll.Template/IncludeImportMap.ps1
+++ Types/PSJekyll.Template/IncludeImportMap.ps1
@@ -6,7 +6,9 @@
     
     This will add a script tag with the import map.  It should be located in the head of the site.
 
-    If the importmap parameter is provided, it will be used.  Otherwise, the site.data.imports will be used.
+    An importmap can be defined in the front matter of a page, in the site data, or in the site configuration.
+
+    It may be called either `.imports` or `.importMap`.
 #>
 param(
 $ImportMap
@@ -20,19 +22,30 @@ if ($ImportMap) {
 "</script>"
 } else {
 @'
-{% if site.data.imports %}
-<script type="importmap">
-    {
-        "imports": {
-            {% for eachImport in site.data.imports %}
-                "{{eachImport[0]}}": "{{eachImport[1]}}"{% unless forloop.last %},
-                {% endunless %}
-            {% endfor %}
-        }
-    }    
-</script>
-    
+{% if page.imports %}
+    {% assign importMap = page.imports %}
+{% elsif page.importMap %}
+    {% assign importMap = page.importMap %}
+{% elsif site.imports %}
+    {% assign importMap = site.imports %}
+{% elsif site.importMap %}
+    {% assign importMap = site.importMap %}
+{% elseif site.data.imports %}
+    {% assign importMap = site.data.imports %}
+{% elseif site.data.importMap %}
+    {% assign importMap = site.data.importMap %}
 {% endif %}
-
+{% if importMap %}
+<script type="importmap">
+{
+    "imports": {
+        {% for eachImport in importMap %}
+            "{{eachImport[0]}}": "{{eachImport[1]}}"{% unless forloop.last %},
+            {% endunless %}
+        {% endfor %}
+    }
+}
+</script>    
+{% endif %}
 '@    
 }
