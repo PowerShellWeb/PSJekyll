25a4e84795bf518649872c988426e7595b91f52d
James Brundage <+@noreply.github.com>
2024-10-08 09:00:38 -0700

    feat: PSJekyll.Template.Include4bitcss ( Fixes #63 )
    Allowing page level palettes, adding id.

--- Types/PSJekyll.Template/Include4bitcss.ps1
+++ Types/PSJekyll.Template/Include4bitcss.ps1
@@ -5,12 +5,14 @@ $PaletteName
 
 if ($PaletteName) {
 @"
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/2bitdesigns/4bitcss@latest/css/$($PaletteName -replace '\.css$').css" />
+<link rel="stylesheet" id="4bitcss" type="text/css" href="https://cdn.jsdelivr.net/gh/2bitdesigns/4bitcss@latest/css/$($PaletteName -replace '\.css$').css" />
 "@
 } else {
     @(
-        "{% if site.palette %}"
-        '<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/2bitdesigns/4bitcss@latest/css/{{site.palette}}.css" />'
+        "{% if page.palette %}"
+            '<link rel="stylesheet" type="text/css" id="4bitcss" href="https://cdn.jsdelivr.net/gh/2bitdesigns/4bitcss@latest/css/{{page.palette}}.css" />'
+        "{% elsif site.palette %}"
+            '<link rel="stylesheet" type="text/css" id="4bitcss" href="https://cdn.jsdelivr.net/gh/2bitdesigns/4bitcss@latest/css/{{site.palette}}.css" />'
         "{% endif %}"
     ) -join [Environment]::Newline
 }
