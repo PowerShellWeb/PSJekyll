75d0ad6f768d210467565328f1340e99cb8f1132
James Brundage <+@noreply.github.com>
2024-10-08 20:22:28 -0700

    docs: PSJekyll.Template.IncludeMarginFont ( Fixes #69 )
    Renaming, allowing page margins, adding docs

--- Types/PSJekyll.Template/Include.Margin.html.ps1
+++ Types/PSJekyll.Template/Include.Margin.html.ps1
@@ -6,7 +6,12 @@
     
     This will add a style tag with the margin.  It should be located in the head of the site.
 
-    If the margin parameter is provided, it will be used.  Otherwise, the site.margin will be used.
+    If the margin parameter is provided, it will be used.  
+    
+    Otherwise, the `page.margin` or `site.margin` will be used.
+
+    If neither margin exists, it will default to a margin of `1em` for all elements, 
+    and `0.5em` for all elements when the screen is less than `960px`.
 #>
 param(
 # The margin to include.
@@ -18,6 +23,8 @@ if ($margin) {
     "<style type='text/css' id='globalMargin'>$margin</style>" 
 } else {
     @(
+        "{% if page.margin %}"
+            "<style id='globalMargin' type='text/css'>{{page.margin}}</style>"
         "{% if site.margin %}"
             "<style id='globalMargin' type='text/css'>{{site.margin}}</style>"
         "{% else %}"
