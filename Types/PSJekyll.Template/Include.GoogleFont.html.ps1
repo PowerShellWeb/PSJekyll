be8ed72e3ed20212a8aaa05871cbc28ea60b2b7e
James Brundage <+@noreply.github.com>
2024-10-08 20:16:38 -0700

    docs: PSJekyll.Template.IncludeGoogleFont ( Fixes #59 )
    Renaming and allowing page fonts

--- Types/PSJekyll.Template/Include.GoogleFont.html.ps1
+++ Types/PSJekyll.Template/Include.GoogleFont.html.ps1
@@ -4,14 +4,20 @@
 .DESCRIPTION
     Includes a Google Font in the site.
     
-    This will add a link to the Google Font.  It should be located in the head of the site.
+    This will add a link to the Google Font.
+    
+    It can be located within the site or page front matter.
 #>
 
 param(
 # The name of the font to include.
+# If no value is directly provided, it will attempt to find a value in site.googleFont.
+# If no font is found, it will default to Roboto.
 [string]
 $FontName,
 
+# The code font to include.
+# If no code font is provided, it will default to Roboto Mono.
 [string]
 $CodeFont
 )
@@ -19,7 +25,10 @@ $CodeFont
 if ($FontName) {
     "<link href='https://fonts.googleapis.com/css?family=$fontName' rel='stylesheet'>"
 } else {    
-    "{% if site.googleFont %}"
+    "{% if page.googleFont %}"
+        "<link href='https://fonts.googleapis.com/css?family={{page.googleFont}}' rel='stylesheet'>"
+        "<style type='text/css'>body { font-family: '{{page.googleFont}}',sans-serif } </style>"
+    "{% elsif site.googleFont %}"
         "<link href='https://fonts.googleapis.com/css?family={{site.googleFont}}' rel='stylesheet'>"
         "<style type='text/css'>body { font-family: '{{site.googleFont}}',sans-serif } </style>"
     "{% else %}"
@@ -31,7 +40,10 @@ if ($FontName) {
 if ($CodeFont) {
     "<link href='https://fonts.googleapis.com/css?family=$CodeFont' rel='stylesheet'>"
 } else {    
-        "{% if site.codeFont %}"
+        "{% if page.codeFont %}"
+            "<link href='https://fonts.googleapis.com/css?family={{page.codeFont}}' rel='stylesheet'>"
+            "<style type='text/css'>code, pre { font-family: '{{page.codeFont}}',monospace } </style>"
+        "{% elsif site.codeFont %}"
             "<link href='https://fonts.googleapis.com/css?family={{site.codeFont}}' rel='stylesheet'>"
             "<style type='text/css'>code, pre { font-family: '{{site.codeFont}}',monospace } </style>"
         "{% else %}"
