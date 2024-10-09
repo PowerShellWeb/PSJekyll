2de1e43e68c5c65d66066a8b1be5e9890dc26826
James Brundage <+@noreply.github.com>
2024-10-09 15:21:24 -0700

    fix: PSJekyll.Template.Include.PSTypeName ( Fixes #87 )
    Simplifying and fixing pathing

--- Types/PSJekyll.Template/Include.PSTypeName.md.ps1
+++ Types/PSJekyll.Template/Include.PSTypeName.md.ps1
@@ -1,11 +1,7 @@
 param()
 
 @"
-{% for functionName in site.data.PSExportTypeNames %}
-{% if site.pages | where: "url", "/{{functionName}}" %}
-* [{{ functionName }}](/{{functionName}})
-{% else %}
-* {{ functionName }}
-{% endif %}
+{% for typeName in site.data.PSModule.TypeNames %}
+* [{{ typeName }}](/{{typeName}})
 {% endfor %}
 "@
