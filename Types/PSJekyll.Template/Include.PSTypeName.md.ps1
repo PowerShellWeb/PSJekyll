e3b59d515a90c3de7becab5c966a8ad5ff6d3828
James Brundage <+@noreply.github.com>
2024-10-09 13:12:19 -0700

    feat: PSJekyll.Template.IncludePSTypeName ( Fixes #87 )
    Fixing data variable reference

--- Types/PSJekyll.Template/Include.PSTypeName.md.ps1
+++ Types/PSJekyll.Template/Include.PSTypeName.md.ps1
@@ -1,7 +1,7 @@
 param()
 
 @"
-{% for functionName in site.data.PSModuleFunctionNames %}
+{% for functionName in site.data.PSExportTypeNames %}
 {% if site.pages | where: "url", "/{{functionName}}" %}
 * [{{ functionName }}](/{{functionName}})
 {% else %}
