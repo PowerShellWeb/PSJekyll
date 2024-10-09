ba2f65811fe97f1f8d9b842ce30f16ca3981c270
James Brundage <+@noreply.github.com>
2024-10-09 12:33:59 -0700

    docs: PSJekyll.Site.get_File ( Fixes #40 )
    Adding docs

--- Types/PSJekyll.Site/get_File.ps1
+++ Types/PSJekyll.Site/get_File.ps1
@@ -1,3 +1,11 @@
+<#
+.SYNOPSIS
+    Gets the files in a site.
+.DESCRIPTION
+    Gets the files in a Jekyll site.
+.EXAMPLE
+    $psJekyll.CurrentSite.File
+#>
 if ($this.Directory -and -not $this.'.FileList') {
     $this.psobject.properties.add([psnoteproperty]::new('.FileList', @($this.Directory.EnumerateFiles("*",'AllDirectories'))))    
 }
