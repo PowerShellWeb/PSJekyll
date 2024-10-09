5e632d309501cc8b7ed91b2e7150ddf2858c197f
James Brundage <+@noreply.github.com>
2024-10-08 19:52:09 -0700

    docs: PSJekyll.Site.get/set_Config ( Fixes #65, Fixes #66 )
    Adding examples and always using New-Item

--- Types/PSJekyll.Site/set_Config.ps1
+++ Types/PSJekyll.Site/set_Config.ps1
@@ -5,8 +5,16 @@
     Gets the configuration of the Jekyll site.  
     
     This can be provided by a _config.yml file in the root of the site (and essentially marks it as a site)
+.EXAMPLE
+    $psJekyll.CurrentSite.Config = [Ordered]@{
+        title = 'My Awesome Site'
+        description = 'This is a site that is awesome.'
+        permalink = 'pretty'
+    }
 #>
 param(
+# The new configuration object.
+# This will be converted to YAML and added to the _config.yml file.
 $Value
 )
 $configFile = Join-Path $this.Directory "_config.yml"
@@ -20,8 +28,5 @@ $valueToAdd =
     else {
         & $PSJekyll.FormatYAML.Script $value
     }
-if (Test-Path $configFile) {    
-    Set-Content -Path $configFile -Value $valueToAdd    
-} else {
-    New-Item -ItemType File -Path $configFile -Force -Value $valueToAdd
-}
+
+New-Item -ItemType File -Path $configFile -Force -Value $valueToAdd
