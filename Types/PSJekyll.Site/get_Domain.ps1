9645417ac8ad986f1db31d96a26607cd270e40e1
James Brundage <+@noreply.github.com>
2024-10-08 20:54:53 -0700

    docs: PSJekyll.Site.get/set_Domain ( Fixes #61, Fixes #62 )
    Adding examples

--- Types/PSJekyll.Site/get_Domain.ps1
+++ Types/PSJekyll.Site/get_Domain.ps1
@@ -5,6 +5,8 @@
     Gets the domain name of the Jekyll site.  
     
     This can be provided by a CNAME file in the root of the site.
+.EXAMPLE
+    $PSJekyll.CurrentSite.Domain
 #>
 $cNamePath = Join-Path $this.Directory "CNAME"
 if (Test-Path $cNamePath) {
--- Types/PSJekyll.Site/set_Domain.ps1
+++ Types/PSJekyll.Site/set_Domain.ps1
@@ -5,6 +5,10 @@
     Sets the domain name of the Jekyll site.  
     
     This will create a CNAME file in the root of the site.
+
+    This will also attempt to resolve the domain name to ensure it is valid, and will write a warning if it is not.
+.EXAMPLE
+    $PSJekyll.CurrentSite.Domain = 'psjekyll.powershellweb.com'
 #>
 param([string]$cname)
 $cNamePath = Join-Path $this.Directory "CNAME"
