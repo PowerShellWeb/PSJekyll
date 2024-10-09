c680d7a49b1303f71715c2306944d821777dac16
James Brundage <+@noreply.github.com>
2024-10-09 15:48:50 -0700

    fix: PSJekyll.Template.Include.PSTag ( Fixes #82 )
    Updating pathing

--- Types/PSJekyll.Template/Include.PSTag.md.ps1
+++ Types/PSJekyll.Template/Include.PSTag.md.ps1
@@ -1,7 +1,7 @@
 param()
 
 @"
-{% for tagName in site.data.PSModuleInfo.Tags %}
+{% for tagName in site.data.PSModule.Info.Tags %}
 * [{{ tagName }}](https://www.powershellgallery.com/packages?q=Tags%3A%22{{tagName}}%22)
 {% endfor %}
 "@
