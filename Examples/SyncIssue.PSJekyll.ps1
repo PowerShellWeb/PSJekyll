925dc842b001d9d5c0e6455a0d25640ac8ff5d66
James Brundage <+@noreply.github.com>
2024-10-14 20:25:19 -0700

    test: Adding Jitter directly to SyncIssue ( Fixes #91 )

--- Examples/SyncIssue.PSJekyll.ps1
+++ Examples/SyncIssue.PSJekyll.ps1
@@ -2,6 +2,9 @@ if (-not $gitHubEvent.issue -and -not $gitHubEvent.discussion) {
     Write-Warning "GitHub Event is not an issue or discussion, will not sync"
 }
 
+Start-Sleep -Milliseconds (Get-Random -Minimum 3000 -Maximum 15000)
+$null = git pull
+
 if ($gitHubEvent.issue) {
     $psJekyll.CurrentSite.Data = @{"issues/$($gitHubEvent.issue.number)" = $gitHubEvent.issue}
 } 
