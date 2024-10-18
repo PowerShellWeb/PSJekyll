ec398e172a36115534f9dcf19294ba93d59c4481
James Brundage <+@noreply.github.com>
2024-10-17 18:57:21 -0700

    test: SyncIssue with token ( re #91 ) [skip ci]

--- Examples/SyncIssue.PSJekyll.ps1
+++ Examples/SyncIssue.PSJekyll.ps1
@@ -6,11 +6,16 @@ Start-Sleep -Seconds (Get-Random -Minimum 10 -Maximum 20)
 get merge origin/main | Out-Host
 git pull | Out-Host
 
-if ($gitHubEvent.issue) {
-    $psJekyll.CurrentSite.Data = @{"issues/$($gitHubEvent.issue.number)" = $gitHubEvent.issue}
-} 
-if ($gitHubEvent.discussion) {
-    $psJekyll.CurrentSite.Data = @{"discussions/$($gitHubEvent.discussion.number)" = $gitHubEvent.discussion}
+if ($GitHubToken -and $env:GIHUB_REPOSIORY) {
+    $owner, $repo = $env:GIHUB_REPOSIORY -split '/'
+    "Getting issues for $owner, $repo" | Out-Host
+} else {
+    if ($gitHubEvent.issue) {
+        $psJekyll.CurrentSite.Data = @{"issues/$($gitHubEvent.issue.number)" = $gitHubEvent.issue}
+    } 
+    if ($gitHubEvent.discussion) {
+        $psJekyll.CurrentSite.Data = @{"discussions/$($gitHubEvent.discussion.number)" = $gitHubEvent.discussion}
+    }
 }
 
 $psJekyll.CurrentSite.Data
