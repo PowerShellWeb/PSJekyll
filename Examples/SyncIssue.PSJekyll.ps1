3c522bb6b4a1044c8954e239627f7266c4893a82
James Brundage <+@noreply.github.com>
2024-10-17 19:48:19 -0700

    test: SyncIssue with token ( re #91 ) [skip ci]
    Syncing the top 100 issues, fixing parameter binding

--- Examples/SyncIssue.PSJekyll.ps1
+++ Examples/SyncIssue.PSJekyll.ps1
@@ -1,17 +1,40 @@
+param(
+# The issue state.  Can be open, closed, or all
+[ValidateSet('open','closed','all')]
+[string]
+$IssueState = $(if ($env:IssueState) { $env:IssueState } else { 'all' }),
+
+# The issue label.
+[string[]]
+$IssueLabel = $(if ($env:IssueLabel) { $env:IssueLabel } else { @() })
+)
+
 if (-not $gitHubEvent.issue -and -not $gitHubEvent.discussion) {
     Write-Warning "GitHub Event is not an issue or discussion, will not sync"
 }
 
-Start-Sleep -Seconds (Get-Random -Minimum 10 -Maximum 20)
-git merge origin/main | Out-Host
-git pull | Out-Host
-
-if ($GitHubToken) {
+if ($env:GitHubToken) {
     "GitHub Token is present" | Out-Host
 }
-if ($GitHubToken -and $env:GIHUB_REPOSIORY) {
-    $owner, $repo = $env:GIHUB_REPOSIORY -split '/'
+if ($GitHubToken -and $env:GITHUB_REPOSIORY) {
+    $owner, $repo = $env:GITHUB_REPOSIORY -split '/'
     "Getting issues for $owner, $repo" | Out-Host
+    $queryString = @(
+        if ($IssueState) {
+            "state=$($issueState.ToLower())"
+        }
+        if ($IssueLabel) {
+            "labels=$($IssueLabel -join ',')"
+        }
+        'per_page=100'
+    ) -join '&'
+    $issues = 
+        Invoke-RestMethod -Uri "https://api.github.com/repos/$owner/$repo/issues?$queryString" -Headers @{
+            "Authorization" = "token $GitHubToken"
+        }
+    foreach ($issue in $issues) {
+        $psJekyll.CurrentSite.Data = @{"issues/$($issue.number)" = $issue}
+    }
 } else {
     if ($gitHubEvent.issue) {
         $psJekyll.CurrentSite.Data = @{"issues/$($gitHubEvent.issue.number)" = $gitHubEvent.issue}
